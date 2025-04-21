91deadc87984afdcca94b82027947021b956d22c
Zextir <92911802+Zextir@users.noreply.github.com>
2025-04-21 17:39:05 +0200

    WIP

---
+++ Assets/Scripts/SceneTranstionManager.cs
@@ -0,0 +1,5 @@
+// SceneTransitionManager.cs (just exists in the project)
+public static class SceneTransitionManager
+{
+    public static string SpawnPointName = "DefaultSpawn";  // fallback if not set
+}
