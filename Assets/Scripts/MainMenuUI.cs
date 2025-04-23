8ba6323f733555112948bc42805de3103a382d69
David W-S <dws@davidws.com>
2025-04-23 16:44:30 +0200

    Main Menu UI

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -9,4 +9,36 @@ public class MainMenuUI : MonoBehaviour
     {
         SceneManager.LoadSceneAsync("Level1");
     }
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public GameObject allOptions; // assign 'All Options' in the Inspector for the Main Camera
+    public GameObject credits;    // assign 'Credits' in the Inspector for the Main Camera
+
+    public void ShowCreditsAndHideOptions()
+    {
+        if (allOptions != null)
+            allOptions.SetActive(false);
+
+        if (credits != null)
+            credits.SetActive(true);
+    }
+
+    public void ShowOptionsAndHideCredits()
+    {
+        if (allOptions != null)
+            allOptions.SetActive(true);
+
+        if (credits != null)
+            credits.SetActive(false);
+    }
+
 }
+
+
